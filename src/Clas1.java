09c524dd69c8963f3427285e673d9ecd1698b4cd
munzir88 <munzirrrarzykulov01@gmail.com>
2022-11-08 19:42:47 +0600

    munza

---
+++ src/Clas1.java
@@ -0,0 +1,3 @@
+public class Clas1 {
+    String string ;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("привет мир !");
+        System.out.println(" ");
+        Clas1 clas1 = new Clas1();
+        clas1.string = "Hello ";
+
     }}
